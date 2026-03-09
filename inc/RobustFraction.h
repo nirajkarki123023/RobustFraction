5abbaa327fef6255300dfa29c98ef23904880c3d
Niraj Karki <nirajjungkarki@mail.weber.edu>
2026-03-09 01:43:33 +0000

    Refactor Fraction class: Implemented constructors, operator overloads, and simplification logic

--- inc/RobustFraction.h
+++ inc/RobustFraction.h
@@ -6,79 +6,128 @@ using namespace std;
 
 class Fraction {   
     private:
-    //TODO: Create the following in the private access modifier:
-    //      int numerator
-    //      int denominator
+    // numerator and denominator store the value of the fraction.
+    // Keeping them private prevents outside code from changing them directly.
+    int numerator;
+    int denominator;
+
+    // helper function that computes the greatest common divisor using recursion
     int greatestCommonDivisor(int a, int b) {
         if (b == 0)
             return abs(a);
         return abs(greatestCommonDivisor(b, a % b));
     }
+
+    // reduce the fraction to lowest terms and keep the sign on the numerator
     void simplify() {
-        if (denominator < 0) {
+        if (denominator < 0) {              // move any negative sign to the top
             numerator *= -1;
+            denominator *= -1;
         }
 
         int gcd = greatestCommonDivisor(numerator, denominator);
-        numerator /= gcd;
-        denominator /= gcd;
+        if (gcd != 0) {
+            numerator /= gcd;
+            denominator /= gcd;
+        }
     }
 
     public:
-    //TODO: Create a default constructor that takes no parameters and does not set any values
-    //TODO: Create another constructor that takes two parameters: int numerator & int denominator
-    //      set the former equal to numerator and the latter to denominator
+    // default constructor (no initialization) – this is what the original assignment asked for
+    // since the object is often immediately filled by operator>>, we leave the members alone.
+    Fraction() { }
 
-    //TODO: now overload the << operator. Remember, operator<< is a friend to the class. Pass ostream& out as
-    //      the first parameter and Fraction& fraction as the second parameter
-    //      
-    //      If the fraction denominator == 1 then you will just need to out the numerator.
-    //      If the numerator is larger than the denominator, then you will need to make it a mixed fraction.
-    //      For example, 4/3 would be 1 and 1/3.
-    //      If the numerator is less than the denominator then just out the numerator followed by '/' and then the
-    //      denominator. Finally return out.
+    // constructor that accepts a numerator and denominator
+    Fraction(int num, int den) : numerator(num), denominator(den) {
+        if (denominator == 0) {
+            cerr << "Denominator cannot be zero. Using 1 instead.\n";
+            denominator = 1;
+        }
+        simplify();       // always keep the fraction reduced
+    }
 
+    // output operator – prints the fraction in a human friendly format
+    friend ostream& operator<<(ostream& out, const Fraction& fraction) {
+        // if denominator is 1 just print the whole number
+        if (fraction.denominator == 1) {
+            out << fraction.numerator;
+            return out;
+        }
+
+        int num = fraction.numerator;
+        int den = fraction.denominator;
+
+        // mixed number when numerator larger than denominator
+        if (abs(num) > den) {
+            int whole = num / den;              // integer part (truncates toward zero)
+            int rem = abs(num % den);           // remainder always positive
+            out << whole << " and " << rem << "/" << den;
+            return out;
+        }
+
+        // proper fraction or negative proper fraction
+        out << num << "/" << den;
+        return out;
+    }
     friend istream& operator>>(istream& in, Fraction& fraction) {
-        //TODO: Now we need to overload operator>>. This is also a friend to the class. Start by prompting
-        //      the user to enter a fraction in the format of n/d
-        //      You will then need to in the numerator, slash, and then the denominator
-        //      If the denominator == 0 then tell the user that the denominator cannot be zero and
-        //      set it to 1. Finally, return in
+        // ask for input only when reading from cin; stringstreams do not need a prompt
+        if (&in == &cin) {
+            cout << "Enter a fraction in the form n/d: ";
+        }
+
+        int num, den;
+        char slash;
+        in >> num >> slash >> den;   // read something like 5/6
+
+        fraction.numerator = num;
+        fraction.denominator = den;
+
+        if (fraction.denominator == 0) {
+            cerr << "Denominator cannot be zero. Using 1 instead.\n";
+            fraction.denominator = 1;
+        }
+
+        fraction.simplify();
+        return in;
     }
 
     friend Fraction operator+(Fraction op1, Fraction op2) {
-        //TODO: Now overload operator+. To do this, you need to use the following formula:
-        //      a/b + c/d
-        //      denominator = b * d
-        //      numerator = (a * d) + (c * b)
-        //      You will then need to make a new fraction object and pass in the new numerator and new
-        //      denominator. Call the simplify() function on the new fraction object and then return it.
+        // a/b + c/d = (a*d + c*b) / (b*d)
+        int num = op1.numerator * op2.denominator + op2.numerator * op1.denominator;
+        int den = op1.denominator * op2.denominator;
+        Fraction result(num, den);
+        result.simplify();
+        return result;
     }
 
-    //TODO: We will now need to overload operator-. The set up will be similar the operator+ function.
-    //      The formula will be the following:
-    //      a/b - c/d
-    //      denominator = a * d
-    //      numerator = (a * d) - (c * b)
-    //      You will once again need to call a fraction object and pass the new numerator and denominator
-    //      Remember to call simplify() and return the new object.
+    // subtraction operator
+    friend Fraction operator-(Fraction op1, Fraction op2) {
+        // a/b - c/d = (a*d - c*b) / (b*d)
+        int num = op1.numerator * op2.denominator - op2.numerator * op1.denominator;
+        int den = op1.denominator * op2.denominator;
+        Fraction result(num, den);
+        result.simplify();
+        return result;
+    }
 
     friend Fraction operator*(Fraction op1, Fraction op2) {
-        //TODO: The formula for this one will be the following:
-        //      a/b * c/d
-        //      denominator = b * d
-        //      numerator = a * c
-        //      Call a new fraction object and pass the numerator and the denominator
-        //      Simplify the fraction and return the object.
+        // a/b * c/d = (a*c) / (b*d)
+        int num = op1.numerator * op2.numerator;
+        int den = op1.denominator * op2.denominator;
+        Fraction result(num, den);
+        result.simplify();
+        return result;
     }
 
-    //TODO: We will now overload the operator/. It will look similar to the function above.
-    //      a/b / c/d
-    //      denominator = b * c
-    //      numerator = a * d
-    //      Create a new fraction object and pass the numerator and the denominator and simplify the fraction
-    //      return the new object
-
+    // division operator
+    friend Fraction operator/(Fraction op1, Fraction op2) {
+        // a/b ÷ c/d = (a*d) / (b*c)
+        int num = op1.numerator * op2.denominator;
+        int den = op1.denominator * op2.numerator;
+        Fraction result(num, den);
+        result.simplify();
+        return result;
+    }
     operator double() {
         return static_cast<double>(numerator) / denominator;
     }
